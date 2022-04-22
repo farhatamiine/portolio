cb5b5d6fc837bc6b3984e404b8e2db050f8c516d
farhatamiine <farhatamiine@gmail.com>
2022-04-22 23:12:37 +0000

    Initial Commit

---
+++ components/header.tsx
@@ -0,0 +1,96 @@
+import React, { FC } from 'react'
+
+export const Header = () => {
+  return (
+    <>
+      <header className="py-6">
+        <div className="container mx-auto flex w-full items-center justify-between px-8 md:px-14 lg:px-24">
+          <div className="text-lg font-bold">Amine Farhat</div>
+          <div className="hidden items-center space-x-12 md:flex">
+            <a href="#" className="text-selected-text">
+              Home
+            </a>
+            <a href="#work">My work</a>
+            <a href="#clients">Clients</a>
+            <a href="#hire">
+              <button className="bg-theme px-6 py-2 font-bold">
+                Contact Me
+              </button>
+            </a>
+          </div>
+          <div className="md:hidden">
+            <svg
+              width="26"
+              height="18"
+              viewBox="0 0 26 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M13 17.5H0.25V14.6667H13V17.5ZM25.75 10.4167H0.25V7.58333H25.75V10.4167ZM25.75 3.33333H13V0.5H25.75V3.33333Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+        </div>
+      </header>
+      <div className="container mx-auto mt-16 flex w-full items-center justify-between px-8 md:px-14 lg:px-24">
+        <div className="flex flex-wrap md:flex-nowrap">
+          <nav className="left-percentage fixed inline-block hidden lg:mr-24 lg:w-4 xl:block">
+            <div className="left-50 absolute mt-36 -translate-x-1/2 transform space-y-6">
+              <a
+                href="#"
+                className="nav-dot selected-circle block h-7 w-7 rounded-full border-4 border-nav bg-body"
+              >
+                <span className="ml-10 rounded-md bg-black px-2 py-1 opacity-0">
+                  Home
+                </span>
+              </a>
+              <a
+                href="#work"
+                className="nav-dot block h-7 w-7 rounded-full border-4 border-nav bg-body"
+              >
+                <span className="ml-10 rounded-md bg-black px-2 py-1 opacity-0">
+                  Work
+                </span>
+              </a>
+              <a
+                href="#clients"
+                className="nav-dot block h-7 w-7 rounded-full border-4 border-nav bg-body"
+              >
+                <span className="ml-10 rounded-md bg-black px-2 py-1 opacity-0">
+                  Experience
+                </span>
+              </a>
+              <a
+                href="#hire"
+                className="nav-dot block h-7 w-7 rounded-full border-4 border-nav bg-body"
+              >
+                <span className="ml-10 rounded-md bg-black px-2 py-1 opacity-0">
+                  Contact
+                </span>
+              </a>
+            </div>
+          </nav>
+
+          <div className="mt-0 flex max-w-xl flex-wrap justify-center md:my-36 md:justify-start lg:ml-20">
+            <h1 className="text-center text-5xl font-bold md:text-left md:text-6xl lg:text-7xl">
+              Building
+              <br /> beautiful web experiences.
+            </h1>
+            <div className="flex w-full justify-center md:justify-start">
+              <button className="mt-12 flex items-center space-x-3 bg-theme px-8 py-4 font-bold text-white">
+                <span>View my work.</span>
+              </button>
+            </div>
+          </div>
+          <img
+            src="../img/man.jpg"
+            alt="Man"
+            className="-z-1 right-0 mt-12 -mt-6 w-3/4 md:absolute md:mt-0"
+          />
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ components/work.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default function Work() {
+  return (
+    <div className="container mx-auto mt-64 flex w-full items-center justify-between px-8 md:px-14 lg:px-24">
+      <section className="w-full">
+        <h2 id="work" className="secondary-title">
+          My work
+        </h2>
+        <p className="section-paragraph">
+          I’ve had the pleasure of working with multiple Fortune 500 companies,
+          designing and implementing both frontend and backend.
+        </p>
+
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          <img
+            src="https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+            className="h-36 w-full bg-nav object-cover lg:h-72"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1545235617-9465d2a55698?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+            className="h-36 w-full bg-nav object-cover lg:h-72"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1586717791821-3f44a563fa4c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+            className="hidden h-36 w-full bg-nav object-cover md:block lg:h-72"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1558655146-d09347e92766?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80"
+            className="hidden h-36 w-full bg-nav object-cover md:block lg:h-72"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1547119957-637f8679db1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+            className="hidden h-36 w-full bg-nav object-cover md:block lg:h-72"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1559028012-481c04fa702d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1336&q=80"
+            className="hidden h-36 w-full bg-nav object-cover md:block lg:h-72"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1603969072881-b0fc7f3d77d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80s"
+            className="hidden h-36 w-full bg-nav object-cover md:block lg:h-72"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1618761714954-0b8cd0026356?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            className="hidden h-36 w-full bg-nav object-cover md:block lg:h-72"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1545235617-7a424c1a60cc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80"
+            className="hidden h-36 w-full bg-nav object-cover md:col-span-2 md:block lg:col-span-1 lg:h-72"
+          />
+        </div>
+      </section>
+    </div>
+  )
+}
