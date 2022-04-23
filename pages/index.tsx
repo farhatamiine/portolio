37189c9e87eb4ce5ddc382a23333cfb32738a0a3
farhatamiine <farhatamiine@gmail.com>
2022-04-23 12:29:39 +0000

    Fix type variable

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Head from 'next/head'
 import Script from 'next/script'
 import Client from '../components/client'
 import Contact from '../components/contact'
-import { gql, GraphQLClient } from 'graphql-request'
+import {gql, GraphQLClient} from 'graphql-request'
 
-import { Header } from '../components/header'
+import {Header} from '../components/header'
 import Work from '../components/work'
 
 export const getStaticProps = async () => {
-  const GRAPHQL_ENDPOINT: string =
-    'https://api-eu-central-1.graphcms.com/v2/ckrbecswj0n2e01xpfi9v8tme/master'
+    const GRAPHQL_ENDPOINT: string =
+        'https://api-eu-central-1.graphcms.com/v2/ckrbecswj0n2e01xpfi9v8tme/master'
 
-  const graphQlClient = new GraphQLClient(GRAPHQL_ENDPOINT)
-  const query = gql`
+    const graphQlClient = new GraphQLClient(GRAPHQL_ENDPOINT)
+    const query = gql`
     {
       experienceSections {
         sectionTitle
@@ -30,32 +30,34 @@ export const getStaticProps = async () => {
       }
     }
   `
-  const data = await graphQlClient.request(query)
-  return {
-    props: {
-      data,
-    },
-  }
+    const data = await graphQlClient.request(query)
+    return {
+        props: {
+            data,
+        },
+    }
 }
 
 interface PropType {
-  data: []
+    data: []
 }
-const Home: NextPage<PropType> = ({ data }: PropType) => {
-  const { experienceSections } = data
-  return (
-    <>
-      <Head>
-        <title>Amine Farhat - Full Stack Developer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <Work />
-      <Client experienceSection={experienceSections} />
-      <Contact />
-      <Script src="https://res.cloudinary.com/diab-control/raw/upload/v1650669449/main_y0silh.js"></Script>
-    </>
-  )
+
+const Home: NextPage<PropType> = ({data}: PropType) => {
+    // @ts-ignore
+    const {experienceSections} = data
+    return (
+        <>
+            <Head>
+                <title>Amine Farhat - Full Stack Developer</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <Header/>
+            <Work/>
+            <Client experienceSection={experienceSections}/>
+            <Contact/>
+            <Script src="https://res.cloudinary.com/diab-control/raw/upload/v1650669449/main_y0silh.js"/>
+        </>
+    )
 }
 
 export default Home
